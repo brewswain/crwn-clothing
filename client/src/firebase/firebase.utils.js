ca851f4fd32f04f0cbc0f11b1491822da2ced2d3
brewswain <brandonlee2543@hotmail.com>
2019-12-02 07:55:51 -0400

    tried remaking entire repo

--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -3,13 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+   apiKey: "AIzaSyB6fy-XH-UzNillUf2u0-1Kme7htr_CZbk",
+    authDomain: "crwn-db-4603d.firebaseapp.com",
+    databaseURL: "https://crwn-db-4603d.firebaseio.com",
+    projectId: "crwn-db-4603d",
+    storageBucket: "crwn-db-4603d.appspot.com",
+    messagingSenderId: "570303283013",
+    appId: "1:570303283013:web:74e60ac6fb121aed85c82c",
+    measurementId: "G-7J8XZWLCXS"
 };
 
 firebase.initializeApp(config);
